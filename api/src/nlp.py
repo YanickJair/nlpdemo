6ca6247b36de2c08de958a356c6473e30b96cb66
Yanick Andrade <yanick.jair.ta@gmail.com>
2020-09-26 06:37:49 +0100

    working on word analogy

--- api/src/nlp.py
+++ api/src/nlp.py
@@ -1,7 +1,11 @@
 import time
 
+import numpy as np
+
 from spacy.tokens import Span
 from spacy.matcher import PhraseMatcher
+
+from annoy import AnnoyIndex
 
 time_spent = {}
 labels = []
@@ -115,3 +119,83 @@ def answer_question(question=None, TEXT=None, tokenizer=None, model=None):
         raise
     return answer
 
+""" 
+Word Analogy Class
+"""
+class PreTrainedEmbeddings(object):
+    def __init__(self, word_to_index, word_vectors):
+        """ 
+        Args:
+            word_to_index (dict)
+            word_vectors (nupmy.array)
+        """
+        self.word_to_index = word_to_index
+        self.word_vectors = word_vectors
+
+        self.index_to_word = { v: k for k in self.word_to_index.items() }
+        self.index = AnnoyIndex(len(word_vectors[0]), metric='euclidean')
+
+        for _, i in self.word_to_index.items():
+            self.index.add_item(i, self.word_vectors[i])
+        self.index.build(50)
+
+    @classmethod
+    def from_embeddings_file(cls, embedding_file):
+        """ 
+        Args:
+            embedding_file (str) -> path to the csv file
+        """
+        word_to_index = {}
+        word_vectors = []
+        with open(embedding_file) as fp:
+            for line in fp.readlines():
+                line = line.split(" ")
+                word = line[0]
+                vec = np.array([ float(x) for x in line[1:] ])
+                
+                word_to_index[word] = len(word_to_index)
+                word_vectors.append(vec)
+        return cls(word_to_index, word_vectors)
+
+    def get_embedding(self, word):
+        """
+        Args:
+            word (str)
+        Returns:
+            numpy.array
+        """
+        return self.word_vectors[self.word_to_index[word]]
+
+    def get_closest_to_vector(self, vector, n=1):
+        """Given a vector, return its n nearest neighbors
+        Args:
+            vector (np.array)
+            n (int) -> the number of neighbors to return
+        Returns:
+            [str,str...] -> words nearest to the given vector
+        """
+        nn_indices = self.index.get_nns_by_vector(vector, n)
+        return [self.index_to_word[neighbor] for neighbor in nn_indices]
+    
+    def compute_analogy(self, word1, word2, word3):
+        """
+        Args:
+            word1 (str)
+            word2 (str)
+            word3 (str)
+        Returns:
+            closest_words (list) -> a list of nearest neighbors
+        """
+        vec1 = self.get_embedding(word1)
+        vec2 = self.get_embedding(word2)
+        vec3 = self.get_embedding(word3)
+        
+        # Symple hypothesis: Analogy is a spatial relationship
+        spatial_relationship = vec2 - vec1
+        vec4 = vec3 + spatial_relationship
+        
+        closest_words = self.get_closest_to_vector(vec4, n=4)
+        existng_words = set([word1, word2, word3])
+        closest_words = [word for word in closest_words if word not in existng_words]
+        
+        return closest_words
