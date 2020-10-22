9c38a45095804ead684875354469c253840befb9
Yanick Andrade <yanick.jair.ta@gmail.com>
2020-10-22 21:52:45 +0100

    deploy

---
+++ precache-manifest.d2140dde61e60e77b9d4eb87a33fde25.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50156c71fb2cf47d5cfe134231975922",
+    "url": "/nlpdemo/270e21a9.png"
+  },
+  {
+    "revision": "db602d9e7ec31065dee85f3539e0b259",
+    "url": "/nlpdemo/Website Yanick-08.png"
+  },
+  {
+    "revision": "9a17835152b0e184006aa8d4e7b046d5",
+    "url": "/nlpdemo/Yanick-base-03.png"
+  },
+  {
+    "revision": "a16960d6adb0a0d69696",
+    "url": "/nlpdemo/css/app.0e99e8ea.css"
+  },
+  {
+    "revision": "11cee5a8e4c5f2a553ef",
+    "url": "/nlpdemo/css/chunk-vendors.bb49d299.css"
+  },
+  {
+    "revision": "815dcddfb14aed587146240d58babeb2",
+    "url": "/nlpdemo/index.html"
+  },
+  {
+    "revision": "a16960d6adb0a0d69696",
+    "url": "/nlpdemo/js/app.c92104e8.js"
+  },
+  {
+    "revision": "11cee5a8e4c5f2a553ef",
+    "url": "/nlpdemo/js/chunk-vendors.9ec0242f.js"
+  },
+  {
+    "revision": "7962b4da7934e4d5f87a0aea75fd1a19",
+    "url": "/nlpdemo/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/nlpdemo/robots.txt"
+  }
+]);
